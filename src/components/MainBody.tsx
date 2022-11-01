17fe0bee633878780eed0687fc19b61e9af0ed65
Kaidan Gustave <kaidangustave@yahoo.com>
2022-11-01 18:05:28 -0500

    small changes to main body sections

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { /* Col, */ Container } from 'react-bootstrap';
 
 type MainBodyProps = React.PropsWithChildren & { className?: string; };
 type MainBodySectionProps = React.PropsWithChildren & {
@@ -17,14 +17,22 @@ export default function MainBody({ children, className }: MainBodyProps) {
 };
 
 MainBody.Section = function (props: MainBodySectionProps) {
-  const { label, headerAlign, className, children } = props;
-  const headerClassName = 'text-center fs-4 my-auto'.concat(headerAlign ? ' my-lg-2' : '');
-  const contentClassName = 'd-flex py-2 flex-column'.concat(className ? ` ${className}` : '');
+  const { label, /* headerAlign, */ className, children } = props;
+  // const headerClassName = 'text-center fs-2 my-auto';
+  // const contentClassName = 'd-flex py-2 flex-column'.concat(className ? ` ${className}` : '');
+  // return (
+  //   <section className="d-flex flex-column flex-lg-row m-2 mx-sm-4">
+  //     <Col as="h1" xs="auto" lg="1" className={headerClassName}>{label}</Col>
+  //     <div className="vr mx-lg-4 my-lg-1" />
+  //     <Col as="div" className={contentClassName}>{children}</Col>
+  //   </section>
+  // );
   return (
-    <section className="d-flex flex-column flex-lg-row m-2 mx-sm-4">
-      <Col as="h1" xs="auto" lg="1" className={headerClassName}>{label}</Col>
-      <div className="vr mx-lg-4 my-lg-1" />
-      <Col as="div" className={contentClassName}>{children}</Col>
-    </section>
+    <div className="d-flex flex-column m-2 mx-sm-4">
+      <h1 className={'text-center fs-2 my-auto'}>{label}</h1>
+      <div className={'d-flex py-2 flex-column'.concat(className ? ` ${className}` : '')}>
+        {children}
+      </div>
+    </div>
   );
 };
